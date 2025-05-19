0e7e74d898cc1400ceb43128d03d7fe63c8702c1
Digvijay Arun Sutar <digvijaysutar1004@gmail.com>
2025-05-19 04:46:14 +0000

    Initial commit

--- Ecom/src/main/java/com/MainApp/EcomApplication.java
+++ Ecom/src/main/java/com/MainApp/EcomApplication.java
@@ -2,12 +2,18 @@ package com.MainApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcomApplication {
+public class EcomApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcomApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EcomApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EcomApplication.class, args);
+    }
 }
